0aafdcb80af72cd03fdb3168d1caeba76c52993a
Mantenimiento <Mantenimiento@192.168.30.250>
2016-03-28 13:10:21 +0200

    032820161310

--- src/Models/Database.java
+++ src/Models/Database.java
@@ -1,5 +1,40 @@
 package Models;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Database {
 
+    private String db = "";
+    private String user = "";
+    private String password = "";
+    private String url = "";
+    private Connection conn = null;
+
+    public void conectar() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        conn = DriverManager.getConnection(this.url, this.user, this.password);
+        System.out.println("Conexion MySQL realizada con exito.");
+    }
+
+    public Connection getConexion() {
+        return this.conn;
+    }
+
+    public void setDatabase(String db) {
+        this.db = db;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setURL(String ip, String puerto) {
+        this.url = "jdbc:mysql://" + ip + ":" + puerto + "/" + db;
+    }
 }
--- src/Models/Model.java
+++ src/Models/Model.java
@@ -1,5 +1,15 @@
 package Models;
 
-public class Model {
+import java.sql.SQLException;
+
+public class Model extends Database {
+
+    public Model(String db, String user, String pass, String ip, String port) throws ClassNotFoundException, SQLException {
+        super.setDatabase(db);
+        super.setUser(user);
+        super.setPassword(pass);
+        super.setURL(ip, port);
+        super.conectar();
+    }
 
 }
